9d173f863efcd638d100c300e1e78cac3887e524
Will Smith <william.smith@helmholtz-berlin.de>
2021-10-27 14:19:58 +0200

    added name return and eLog writing function, added more error checking

--- bessyii/eLog.py
+++ bessyii/eLog.py
@@ -3,7 +3,12 @@ import Levenshtein as lev
 
 def requestInvestigationName(username,password):
 
-    session_id = getSessionID(username,password)
+    session_id, full_name = getSessionID(username,password)
+    
+    if session_id == None:
+        
+        print("Authentication Failed\n")
+        return None, None, None
 
     url = 'https://icat.helmholtz-berlin.de/icatplus/catalogue/'+session_id+'/investigation'
     header = {'Content-Type': 'application/json'}
@@ -46,7 +51,7 @@ def requestInvestigationName(username,password):
             if part_title == 'exit':
                 return None, None, None
  
-    return title, name, id_num
+    return title, name, id_num, full_name
 
 import requests
 import logging
@@ -87,12 +92,63 @@ def getSessionID(username, password):
     proxies = {"http": "http://www.bessy.de:3128", "https": "http://www.bessy.de:3128"}
     header = {'Content-Type': 'application/json'}
     data = {"plugin":"hzbrex","username":username,"password":password}
-    response = requests.post('https://icat.helmholtz-berlin.de/icatplus/session', headers=header,data=json.dumps(data), proxies=proxies)
-    response_data = json.loads(response.text)
-    session_id = response_data['sessionId']
-    return session_id
+    
+    code = 403
+    while code != 200:
+        try:
+            response = requests.post('https://icat.helmholtz-berlin.de/icatplus/session', headers=header,data=json.dumps(data), proxies=proxies)
+            code = response.status_code
+            if code == 403:
+        
+                print("Authentication Failed")
+                username =str(input("username: "))
+                password = str(getpass())
+                data = {"plugin":"hzbrex","username":username,"password":password}
+                        
+            elif code != 200:
+                print(f"error code= {code}, retrying")
+                response.raise_for_status()
+
+            else:
+                
+                response_data = json.loads(response.text)
+                
+                session_id = response_data['sessionId']
+                full_name = response_data['fullName']
+                return session_id, full_name
+        
+        except:
+             pass
 
 
+def writeToELog(message, username, password, investigation_id ):
+    # make a new comment with the machine tag
+    now = datetime.now()-timedelta(hours=2)
 
+    dt_string = now.strftime("%m/%d/%Y %H:%M:%S") # Note the American format.
+    html_event_with_tag ={
+    "type":"annotation",
+    "category":"comment",
+    "content":[
+        {
+            "text":"<p>"+str(message)+"</p>",
+            "format":"html"
+    }],
+    "creationDate":dt_string,
+    "tag": [
+    "60deb980e105e3001a3daa2a"
+    ]
+    } # The tag is used to write "machine"
+
+    #get session id
+    proxies = {"http": "http://www.bessy.de:3128", "https": "http://www.bessy.de:3128"}
+    session_id, full_name = getSessionID(username,password)
+
+    url = 'https://icat.helmholtz-berlin.de/icatplus/logbook/' + session_id + '/investigation/id/'+ str(investigation_id)+ '/event/create'
+    header = {'Content-Type': 'application/json'}
+    data = html_event_with_tag
+    response = requests.post(url, headers=header,data=json.dumps(data), proxies=proxies)
+
+    return response.status_code
 
 
