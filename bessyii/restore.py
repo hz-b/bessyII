0d5adc917516d197eed36244d07bb5714e06c572
Will Smith <william.smith@helmholtz-berlin.de>
2022-02-24 07:52:10 +0100

    initial commit of restore

---
+++ bessyii/restore.py
@@ -0,0 +1,136 @@
+from bluesky.plan_stubs import mv
+from bluesky.utils import (
+    separate_devices,
+    all_safe_rewind,
+    Msg,
+    ensure_generator,
+    short_uid as _short_uid,
+)
+from ophyd import PVPositioner, PositionerBase
+
+import bluesky.preprocessors as bpp
+from bluesky.protocols import Status
+
+def restore(baseline_stream, devices, md=None):
+    """
+    
+    Restore the a set of devices to setpoint values defined in a baseline.
+    The values taken at the start of the run are taken. 
+    Configuration of the devices is also restored
+    
+
+    Parameters
+    ----------
+    baseline_stream : baseline stream 
+        the baseline stream we want to restore from e.g db[-1].baseline
+    devices : a list of devices
+        the devices we are going to restore        
+    md : dict, optional
+        metadata
+
+   
+    """
+    
+    _md = {'identifier': baseline_stream.metadata['start']['uid'],
+           'plan_name': 'restore',
+           'hints': {}
+           }
+    _md.update(md or {})
+          
+    
+    baseline_data = baseline_stream.read()
+    @bpp.run_decorator(md=_md)
+    def inner_restore():
+        status_objects = []
+        
+        #Restore Configuration
+        for device in devices:
+            
+            #Check that this device does not have a parent in the list of devices
+            if device.parent not in devices:
+                
+                #find the name of the device containing this device in the list
+                
+                if device.parent == None: # if this device is a top level parent then we can only configure it if it's in the baseline
+                    
+                    #get the configuration x_array from the baseline
+                    configuration = baseline_stream.config[device.name].read()
+                
+                else:   # this is a child without parents in the list (but possibly parents in the baseline)
+                    
+                    list_of_parents_of_device = [device.parent]
+                    current_parent = device.parent
+                    while current_parent.parent != None:
+                        current_parent = current_parent.parent
+                        list_of_parents_of_device.append(current_parent)
+                        
+                    list_of_devices_in_baseline =[]
+                    for k,v in baseline_stream.config.items():
+                        list_of_devices_in_baseline.append(k)
+                        
+                    #find if there is a parent in the baseline
+                    found_config = False
+                    for parent_device in list_of_parents_of_device:
+                        
+                        if parent_device.name in list_of_devices_in_baseline:
+                            
+                            configuration = baseline_stream.config[parent_device.name].read()
+                            found_config = True
+                            break
+                            
+                    if not found_config:
+                        
+                        raise KeyError(f"There is no device in the baseline matching {device.name}")
+                        
+
+                #For each configuration attribute in our device, create a dict of the attribute name and the value we need to set it to
+                configuration_dict = {}
+                
+                #create a list of signal names of the top level device
+                name = (device.name+'.')
+                signal_names = []
+                for signal in device.get_instantiated_signals():
+
+                    signal_names.append(signal[0].replace(name,''))
+                    
+                for configuration_attr in device.configuration_attrs:
+                    
+                    #We only want to restore if the attribute is a signal
+                    if configuration_attr in signal_names:
+                        
+                        print(configuration_attr)
+                        
+                        configuration_dict[configuration_attr] = configuration[device.name +'_'+configuration_attr.replace('.','_')].values[0]
+                
+                print(configuration)
+                print(configuration_dict)
+
+                #Perform the configuration for that device
+                device.configure(configuration_dict)
+            
+            
+                     
+        #Restore the setpoint (we will work out readback later)
+        for device in devices:
+            
+            #check that the device is a positioner
+            if isinstance(device,PositionerBase):
+                
+                for attr in device.read_attrs:
+                    if "setpoint" in str(attr):                         
+                        signal_name = device.name + '_'+ attr
+                        print(f"found {signal_name} in baseline")
+
+                        dev_obj = device
+                        setpoint_val = baseline_data[signal_name].values[0]
+                        ret = yield Msg('set', dev_obj, setpoint_val, group = 'restore')
+                        status_objects.append(ret)
+
+        
+        
+        print(f"Restoring devices to run {baseline_stream.metadata['start']['uid']}")
+        yield Msg('wait', None, group='restore')
+
+        return tuple(status_objects)
+
+    return(yield from inner_restore())
---
+++ bessyii/test/test_restore.py
@@ -0,0 +1,308 @@
+import pytest
+from bessyii.restore import restore
+from ophyd.sim import motor, noisy_det
+from ophyd import SoftPositioner, Signal, Device, Component as Cpt
+from bluesky.preprocessors import SupplementalData
+from bluesky import RunEngine
+from databroker.v2 import temp
+
+## Set up env
+
+
+RE = RunEngine({})
+db = temp()
+RE.subscribe(db.v1.insert)
+
+##set up some test devices
+
+from ophyd.sim import SynAxis
+from bessyii_devices.positioners import PVPositionerDone
+
+class SimPositionerDone(SynAxis, PVPositionerDone):
+        
+    def _setup_move(self, position):
+        '''Move and do not wait until motion is complete (asynchronous)'''
+        self.log.debug('%s.setpoint = %s', self.name, position)
+        self.setpoint.put(position)
+        if self.actuate is not None:
+            self.log.debug('%s.actuate = %s', self.name, self.actuate_value)
+            self.actuate.put(self.actuate_value)
+        self._toggle_done()
+    
+
+m1 = SimPositionerDone(name='m1' )
+m2 = SimPositionerDone(name='m2')
+m3 = SimPositionerDone(name='m3')
+from ophyd import EpicsMotor, Signal, Device, Component as Cpt
+class Stage(Device):
+    
+    x = Cpt(SimPositionerDone)
+    y = Cpt(SimPositionerDone)
+    config_param = Cpt(Signal, kind='config')
+    
+class StageOfStage(Device):
+    
+    a = Cpt(Stage)
+    b= Cpt(Stage)
+    config_param = Cpt(Signal, kind='config')
+    
+stage = StageOfStage(name = 'stage')
+
+# Create a baseline
+sd = SupplementalData()
+
+sd.baseline = [m1, m2, stage] 
+RE.preprocessors.append(sd)
+
+# Move the motors to some positions
+
+from bluesky.plan_stubs import mv
+
+config = [1,2,3,4,5,6]
+m1.velocity.set(config[0])
+m2.velocity.set(config[1])
+stage.a.x.velocity.set(config[2])
+stage.b.y.velocity.set(config[3])
+stage.a.config_param.set(config[4])
+stage.config_param.set(config[5])
+
+
+positions = [1.3, 2.1, 2.3, 1.2]
+RE(mv(m1,positions[0], m2, positions[1], stage.a.x, positions[2], stage.b.y, positions[3]))
+
+#record the initial config for later test
+
+
+m1_initial_config_values = {}
+for k, v in m1.read_configuration().items():
+    m1_initial_config_values[k]=v['value']
+
+
+stage_initial_config_values = {}
+for k, v in stage.read_configuration().items():
+    stage_initial_config_values[k]=v['value']
+
+stage_a_initial_config_values = {}
+for k, v in stage.a.read_configuration().items():
+    stage_a_initial_config_values[k]=v['value']
+    
+stage_a_x_initial_config_values = {}
+for k, v in stage.a.x.read_configuration().items():
+    stage_a_x_initial_config_values[k]=v['value']
+    
+#Perform a plan to add something to the databroker
+from bluesky.plans import scan
+
+RE(scan([noisy_det], motor, -1,1,2))
+
+#get the uid
+uid = db[-1].metadata['start']['uid'][:8]
+
+   
+## define the tests
+def test_restore_configuration_parent():
+    
+    #test whether we can restore the configuration of a parent device (and all it's children)
+    
+    
+    #Move the motors to some other positions
+    new_config = [2,3,4,5,6,7]
+    m1.velocity.set(new_config[0])
+    m2.velocity.set(new_config[1])
+    stage.a.x.velocity.set(new_config[2])
+    stage.b.y.velocity.set(new_config[3])
+    stage.a.config_param.set(new_config[4])
+    stage.config_param.set(new_config[5])
+
+    new_positions = [0,0,0,0]
+    RE(mv(m1,new_positions[0], m2, new_positions[1], stage.a.x, new_positions[2], stage.b.y, new_positions[3]))
+
+
+    #Now attempt to restore the original positions
+    baseline_stream = db[uid].baseline
+    
+    device_list = [stage] 
+    #attempt the restore
+    RE(restore(baseline_stream, device_list))
+
+    
+    stage_config_values = {}
+    for k, v in stage.read_configuration().items():
+        stage_config_values[k]=v['value']
+
+    
+    assert stage_config_values == stage_initial_config_values
+    
+## define the tests
+def test_restore_configuration_child_with_parent_and_child():
+    
+    #test whether we can restore the configuration of a parent device (and all it's children), which sits in the middle of a device tree
+    
+    
+    #Move the motors to some other positions
+    new_config = [2,3,4,5,6,7]
+    m1.velocity.set(new_config[0])
+    m2.velocity.set(new_config[1])
+    stage.a.x.velocity.set(new_config[2])
+    stage.b.y.velocity.set(new_config[3])
+    stage.a.config_param.set(new_config[4])
+    stage.config_param.set(new_config[5])
+
+    new_positions = [0,0,0,0]
+    RE(mv(m1,new_positions[0], m2, new_positions[1], stage.a.x, new_positions[2], stage.b.y, new_positions[3]))
+
+
+    #Now attempt to restore the original positions
+    baseline_stream = db[uid].baseline
+    
+    device_list = [stage.a] 
+    #attempt the restore
+    RE(restore(baseline_stream, device_list))
+
+    
+    stage_a_config_values = {}
+    for k, v in stage.a.read_configuration().items():
+        stage_a_config_values[k]=v['value']
+
+    
+    assert stage_a_config_values == stage_a_initial_config_values
+    
+def test_restore_configuration_child():
+    
+    #test whether we can restore the configuration of a child with parents and no children
+    
+    
+    #Move the motors to some other positions
+    new_config = [2,3,4,5,6,7]
+    m1.velocity.set(new_config[0])
+    m2.velocity.set(new_config[1])
+    stage.a.x.velocity.set(new_config[2])
+    stage.b.y.velocity.set(new_config[3])
+    stage.a.config_param.set(new_config[4])
+    stage.config_param.set(new_config[5])
+
+    new_positions = [0,0,0,0]
+    RE(mv(m1,new_positions[0], m2, new_positions[1], stage.a.x, new_positions[2], stage.b.y, new_positions[3]))
+
+
+    #Now attempt to restore the original positions
+    baseline_stream = db[uid].baseline
+    
+    device_list = [stage.a.x] 
+    #attempt the restore
+    RE(restore(baseline_stream, device_list))
+
+    
+    stage_a_x_config_values = {}
+    for k, v in stage.a.x.read_configuration().items():
+        stage_a_x_config_values[k]=v['value']
+
+    assert stage_a_x_config_values == stage_a_x_initial_config_values
+    
+def test_restore_configuration_device():
+    
+    #test whether we can restore the configuration of a single device without children
+    
+    #Move the motors to some other positions
+    new_config = [2,3,4,5,6,7]
+    m1.velocity.set(new_config[0])
+    m2.velocity.set(new_config[1])
+    stage.a.x.velocity.set(new_config[2])
+    stage.b.y.velocity.set(new_config[3])
+    stage.a.config_param.set(new_config[4])
+    stage.config_param.set(new_config[5])
+
+    new_positions = [0,0,0,0]
+    RE(mv(m1,new_positions[0], m2, new_positions[1], stage.a.x, new_positions[2], stage.b.y, new_positions[3]))
+
+
+    #Now attempt to restore the original positions
+    baseline_stream = db[uid].baseline
+    
+    device_list = [m1] 
+    #attempt the restore
+    RE(restore(baseline_stream, device_list))
+
+    
+    m1_config_values = {}
+    for k, v in m1.read_configuration().items():
+        m1_config_values[k]=v['value']
+
+    
+    assert m1_config_values == m1_initial_config_values
+    
+def test_restore_values():
+
+    #Move the motors to some other positions
+    new_config = [2,3,4,5,6,7]
+    m1.velocity.set(new_config[0])
+    m2.velocity.set(new_config[1])
+    stage.a.x.velocity.set(new_config[2])
+    stage.b.y.velocity.set(new_config[3])
+    stage.a.config_param.set(new_config[4])
+    stage.config_param.set(new_config[5])
+
+    new_positions = [0,0,0,0]
+    RE(mv(m1,new_positions[0], m2, new_positions[1], stage.a.x, new_positions[2], stage.b.y, new_positions[3]))
+
+    #Now attempt to restore the original positions
+    baseline_stream = db[uid].baseline
+    
+    device_list = [m1,m2,stage.a.x, stage.b.y]
+    
+    #attempt the restore
+    RE(restore(baseline_stream, device_list))
+    
+    restored_val=[]
+    for device in device_list :
+        
+        restored_val.append(device.readback.get())
+    
+    assert restored_val == positions
+    
+def test_ommit_restore_values():
+    
+    #test that we ONLY restore values of positioners in the list, and not children of devices which are positioners
+    
+    #Move the motors to some other positions
+    new_config = [2,3,4,5,6,7]
+    m1.velocity.set(new_config[0])
+    m2.velocity.set(new_config[1])
+    stage.a.x.velocity.set(new_config[2])
+    stage.b.y.velocity.set(new_config[3])
+    stage.a.config_param.set(new_config[4])
+    stage.config_param.set(new_config[5])
+
+    new_positions = [0,0,0,0]
+    RE(mv(m1,new_positions[0], m2, new_positions[1], stage.a.x, new_positions[2], stage.b.y, new_positions[3]))
+
+    #Now attempt to restore the original positions
+    baseline_stream = db[uid].baseline
+    
+    device_list = [m1,m2,stage ]
+    
+    #attempt the restore
+    RE(restore(baseline_stream, device_list))
+    
+    restored_val=[]
+    for device in [m1,m2,stage.a.x, stage.b.y] :
+        
+        restored_val.append(device.readback.get())
+    
+    test_positions = positions[0:2] + new_positions[2:4]
+    assert restored_val == test_positions   
+
+def test_device_not_in_baseline_values():
+    
+    #test that we fail gracefully if we don't have the device in the baseline
+    
+    #Now attempt to restore the original positions
+    baseline_stream = db[uid].baseline
+    
+    device_list = [m3]
+    
+    #attempt the restore
+    with pytest.raises(KeyError) as e_info:
+        RE(restore(baseline_stream, device_list))
+    
+
