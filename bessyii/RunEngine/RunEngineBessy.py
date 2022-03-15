2be824c6a9568246bf142cc73f1fa7ad14093a57
SV <simone.vadilonga@helmholtz-berlin.de>
2022-03-15 12:09:08 +0100

    add RunEngineBessy and remove bec

--- bessyii/RunEngine/RunEngineBessy.py
+++ bessyii/RunEngine/RunEngineBessy.py
@@ -1,16 +1,23 @@
 from bluesky.run_engine import RunEngine
+from ophyd import Kind        
 
 class RunEngineBessy(RunEngine):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._standard_det = None
-    
-    def add_standard_detectors(self, st_det_list):
-        self._standard_det = st_det_list
+        self._silent_det = []
     
     def __call__(self, *args, **metadata_kw):
-        if self._standard_det != None:
-            for det in self._standard_det:
-                args[0].gi_frame.f_locals['detectors'] += det
-        super().__call__(*args, **metadata_kw)
-        
+        kind_map = {}
+        for det in self._silent_det:
+            if not det in args[0].gi_frame.f_locals['detectors']:
+                kind_map[det.name] = det.val.kind
+                det.val.kind = Kind.normal
+            
+        args[0].gi_frame.f_locals['detectors'] += self._silent_det
+        try:
+            super().__call__(*args, **metadata_kw)
+        finally:
+            for det in self._silent_det:
+                if det.name in kind_map.keys():
+                    det.val.kind = kind_map[det.name]
+
