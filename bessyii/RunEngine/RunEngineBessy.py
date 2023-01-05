7343d17cc02350e5838b022b74513d0d0fb9d276
sissy_user <sissy_user@sissy1-ubuntu-vm.exp.helmholtz-berlin.de>
2023-01-05 16:25:18 +0100

    add command to metadata

--- bessyii/RunEngine/RunEngineBessy.py
+++ bessyii/RunEngine/RunEngineBessy.py
@@ -1,13 +1,13 @@
 from bluesky.run_engine import RunEngine
+import inspect
 
 class RunEngineBessy(RunEngine):
     """RunEngine modifications at BESYYII
 
 
     """    
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
     
     def __call__(self, *args, **metadata_kw):
 
-        metadata_kw.setdefault('commad',inspect.stack()[1].code_context)
+        metadata_kw.setdefault('command',inspect.stack()[1].code_context)
+        super().__call__(*args, **metadata_kw)
