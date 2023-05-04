98d1abafa13a06a15b4866d6aae491fe32018248
Will Smith <william.smith@helmholtz-berlin.de>
2023-05-04 15:03:04 +0200

    updated beam damage reduction

--- bessyii/plans/beam_damage_reduction.py
+++ bessyii/plans/beam_damage_reduction.py
@@ -38,120 +38,9 @@ from bluesky.utils import Msg
 
 from bluesky import preprocessors as bpp
 from bluesky import plan_stubs as bps
-
-def rad_flyscan(detectors, flyer, start=None, stop=None, vel =0.2, delay=0.2,valve=None,*, md=None):
+from bessyii.plans.flying import flyscan
     
-    """
-    count detectors while flying a flyer with start, stop, initial scan velocity, and the delay between det sample time
-
-    The flyer object should implement the ophyd flyer device methods, although we don't use the collect method, instead we poll all of the detectors in the detectors list, as well as the parameters in the read_args of the flyer object.
-
-    Parameters
-    ----------
-    detectors : list
-        list of 'readable' objects
-    flyer :
-        Object of FlyerDevice type
-    start : float
-        The start value fo the flyer
-    stop : float
-        The stop value of the flyer
-    vel : float
-        The initial velocity of the flyer; default is 0.2
-    delay : iterable or scalar, optional
-        Time delay in seconds between successive readings; default is 0.2
-    valve: PVPositioner
-        A positioner that we can move to open and close the beam
-    md : dict, optional
-        metadata
-
-    """
-    
-    # TODO
-    # Add test that detectors is a list longer than 0
-    
-    #Add the flyer to the list of things we want to count
-    detectors_list = detectors.append(flyer)
-    
-    
-    #Define the motor metadata (important for plotting)
-    motor = flyer
-    md = md or {}
-
-    del_req = delay
-
-    if del_req < 0.2:
-        
-        raise ValueError("Sample rate too high.Delay must be >= 0.2 \n")
-
-    flyer.velocity.put(vel)
-
-    md_args = [repr(motor),start,stop,vel,del_req]
-    x_fields = []
-    x_fields.extend(getattr(motor, 'hints', {}).get('fields', []))
-    _md = {'detectors': [det.name for det in detectors],
-           'motors': x_fields,
-           'plan_args': {'detectors': list(map(repr, detectors)),
-                         'motors' : flyer.name,
-                         'start': start, 
-                         'stop' : stop,
-                         'vel': vel,
-                         'delay': del_req,
-                         'args':md_args
-                         },
-
-           'plan_name': 'flycount',
-           'hints': {},
-       }
-    _md.update(md or {})
-    
-    # Deterime the name of the x axis for plotting from the flyer
-    default_dimensions = [(x_fields, 'primary')]
-    default_hints = {}
-    
-    # The following is left from the scan plan implementation, assumes multiple motors
-    if len(x_fields) > 0:
-        default_hints.update(dimensions=default_dimensions)
-
-    # now add default_hints and override any hints from the original md (if
-    # exists)
-    
-    _md['hints'] = default_hints
-    _md.update(md)
-  
-    # Configure the flyer (but don't yet init or start)
-    flyer.start_pos.put(start)
-    flyer.end_pos.put(stop)
-    
-    # Init the run
-    uid = yield from bps.open_run(_md)
-    
-    if valve:
-        yield from abs_set(valve, 1, wait=True)
-        yield from checkpoint()
-
-    # Start the flyer and wait until it's reported that it's started
-    yield from bps.kickoff(flyer, wait=True)
-
-    # Get the status object that tells us when it's done
-    complete_status = yield from bps.complete(flyer, wait=False)
-    
-    
-    while not complete_status.done:
-
-        yield Msg('checkpoint') # allows us to pause the run 
-        yield from bps.one_shot(detectors) #triggers and reads everything in the detectors list
-        yield Msg('sleep', None, del_req)
-
-    if valve:
-        yield from abs_set(valve, 0, wait=True)
-        yield from checkpoint()
-    
-    yield from bps.close_run()
-    return uid
-    
-    
-def beam_reduction_xas_flyer_line(detectors,flyer,energies,motor,start_pos,step_size,num,valve,sleep_time,*,mono_vel=0.2,md=None):
+def beam_reduction_xas_flyer_line(detectors,flyer,energies,motor,start_pos,step_size,num,sleep_time,*,shutter=None, mono_vel=0.2,md=None):
 
     """
     move a motor from a start position in a line at num points, with step_size increment
@@ -176,7 +65,7 @@ def beam_reduction_xas_flyer_line(detectors,flyer,energies,motor,start_pos,step_
         how much should we move the motor at each step. The sign will dictate direction
     num: int
         how many steps should we have in one line? must be a multiple of number of energy ranges
-    valve: PVPositioner
+    shutter: PVPositioner
         A positioner that we can move to open and close the beam
     sleep_time: int
         the time in seconds to wait between moving each 
@@ -193,10 +82,12 @@ def beam_reduction_xas_flyer_line(detectors,flyer,energies,motor,start_pos,step_
     
 
     #close the valve:
-    yield from abs_set(valve, 0, wait=True)
+    print(f"closing {shutter.name}")
+    yield from abs_set(shutter, shutter.close_value, wait=True)
     yield from checkpoint()
     
     #move the sample stage to the start position
+    print(f"moving {motor.name} to {start_pos}")
     yield from abs_set(motor, start_pos, wait=True)
     
     
@@ -204,16 +95,19 @@ def beam_reduction_xas_flyer_line(detectors,flyer,energies,motor,start_pos,step_
 
         for energy in energies:
             #Set the keithley range
+            print(f"setting {detectors[0].name} range to {energy[2]}")
             yield from abs_set(detectors[0].rnge, energy[2], wait=True)
 
             _md = {'i':step}
             _md.update(md or {})
 
             #move the energy to the start with the valve still closed
+            print(f"moving {flyer.name} to {energy[0]}")
             yield from abs_set(flyer, energy[0], wait=True)
 
             #perform the scan, opening and closing the valve
-            yield from rad_flyscan(detectors,flyer,energy[0],energy[1],mono_vel,valve=valve,md=_md)
+            print("Starting flyscan")
+            yield from flyscan(detectors,flyer,energy[0],energy[1],mono_vel,shutter=shutter,md=_md)
             
             #move the motors of the sample stage
             yield from rel_set(motor,step_size, wait=True)
@@ -273,7 +167,7 @@ def valve_open_wrapper(plan, valve):
     return (yield from plan_mangler(plan, insert_open_close))
 
     
-def beam_reduction_xas_stepwise_line(detectors,mono,energies,motor,start_pos,step_size,num,sleep_time,*,valve=None,md=None):
+def beam_reduction_xas_stepwise_line(detectors,mono,energies,motor,start_pos,step_size,num,sleep_time,*,shutter=None,md=None):
 
     """
     move a motor from a start position in a line at num points, with step_size increment
@@ -297,7 +191,7 @@ def beam_reduction_xas_stepwise_line(detectors,mono,energies,motor,start_pos,ste
         how much should we move the motor at each step. The sign will dictate direction
     num: int
         how many steps should we have in one line? must be a multiple of number of energy ranges
-    valve: PVPositioner (optional)
+    shutter: PVPositioner (optional)
         A positioner that we can move to open and close the beam
     sleep_time: int
         the time in seconds to wait between moving each 
@@ -314,8 +208,8 @@ def beam_reduction_xas_stepwise_line(detectors,mono,energies,motor,start_pos,ste
     
 
     #close the valve:
-    if valve:
-        yield from abs_set(valve, 0, wait=True)
+    if shutter:
+        yield from abs_set(shutter, shutter.close_value, wait=True)
         yield from checkpoint()
     
     #move the sample stage to the start position
@@ -337,15 +231,15 @@ def beam_reduction_xas_stepwise_line(detectors,mono,energies,motor,start_pos,ste
             yield from abs_set(mono, energy[0], wait=True)
             
             #perform the scan
-            if valve:
-                yield from valve_open_wrapper(scan(detectors,mono,energy[0],energy[1],num,md=_md),valve)
+            if shutter:
+                yield from valve_open_wrapper(scan(detectors,mono,energy[0],energy[1],num,md=_md), shutter)
             else:
                 yield from scan(detectors,mono,energy[0],energy[1],num,md=_md)
 
             #close the valve:
             
             if valve:
-                yield from abs_set(valve, 0, wait=True)
+                yield from abs_set(shutter, shutter.close_value, wait=True)
                 yield from checkpoint()
     
             #move the motors of the sample stage
